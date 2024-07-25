95a71bf71a89973ff202af5af382855f5e36824a
nikolaybrankov <nikolay.brankov@fortifi.io>
2024-07-25 17:16:16 +0100

    Updated unit tests

--- src/componentContainer/componentContainer.spec.ts
+++ src/componentContainer/componentContainer.spec.ts
@@ -1,193 +1,155 @@
-import { Component } from "../decorators/component.decorator/component.decorator";
-import { Query, QUERY_METADATA_KEY, queryModifierFunction } from "../decorators/query.decorator/query.decorator";
-import { OnChange } from "../interfaces/onChange.interface";
-import { OnDestroy } from "../interfaces/onDestroy.interface";
-import { OnInit } from "../interfaces/onInit.interface";
-import { ComponentContainer } from "./componentContainer";
-import { Injectable } from "../decorators/injectable.decorator/injectable.decorator";
-import { Event, EVENT_METADATA_KEY, eventCallbackSetupFunction } from "../decorators/event.decorator/event.decorator";
-import { IocContainer } from "../iocContainer/IocContainer";
-import { callback } from "../decorators/callback.decorator/callback.decorator";
+import {Component} from "../decorators/component.decorator/component.decorator";
+import {OnChange} from "../interfaces/onChange.interface";
+import {OnInit} from "../interfaces/onInit.interface";
+import {ComponentContainer} from "./componentContainer";
+import {IocContainer} from "../iocContainer/IocContainer";
+import {Mutation, MutationType} from "../interfaces/mutation.interface";
+import {Query, QUERY_METADATA_KEY, queryModifierFunction} from "../decorators/query.decorator/query.decorator";
+import {OnDestroy} from "../interfaces/onDestroy.interface";
+
+const iocContainer = new IocContainer();
+iocContainer.registerArgumentModifier(QUERY_METADATA_KEY, queryModifierFunction);
+
+const setupDomAndContainer = () => {
+    document.body.innerHTML = `
+            <div id="parent">
+                <div id="app">
+                    <div class="test-component" id="one">
+                        <button id="click">Click me</button>
+                    </div>
+                    <div class="test-component" id="two">
+                        <button id="click">Click me</button>
+                    </div>
+                </div>
+            </div>
+        `;
+
+    const rootElement = document.getElementById('app') as HTMLElement;
+    const container = new ComponentContainer(rootElement, iocContainer);
+
+    return { rootElement, container };
+}
 
 describe('ComponentContainer', () => {
-    const iocContainer = new IocContainer();
-    @Injectable({ shared: true })
-    class TestService {
-        public wasInit: boolean = false;
-        public wasDestroyed: boolean = false;
-        public wasChanged: boolean = false;
-        public wasClicked: boolean = false;
-    }
-
-    const service = new TestService();
-
-    iocContainer.registerArgumentModifier(QUERY_METADATA_KEY, queryModifierFunction);
-    iocContainer.registerValue(TestService.name, service);
+    afterEach(() => {
+        document.body.innerHTML = '';
+    });
 
     it('Should fail to instantiate a component container', () => {
         expect(() => new ComponentContainer(document.createElement('div'), iocContainer)).toThrow('Root element does not have a parent node');
     });
 
-    document.body.innerHTML = `
-            <div id="parent">
-                <div id="app">
-                    <div class="test-component">
-                        <button id="click">Click me</button>
-                    </div>
-                    <div class="test-component">
-                        <button id="click">Click me</button>
-                    </div>
-                    <div class="test-component-2" data-name="tester"></div>
-                </div>
-                <div id="app2">
-                    <div class="test-component">
-                        <button id="click">Click me</button>
-                    </div>
-                    <div class="test-component">
-                        <button id="click">Click me</button>
-                    </div>
-                    <div class="test-component-2" data-name="tester"></div>
-                </div>
-                <div id="app3">
-                    <div class="test-component-3"><div>
-                <div>
-            </div>
-        `;
-    const rootElement = document.getElementById('app') as HTMLElement;
-    const container = new ComponentContainer(rootElement, iocContainer);
-    container.registerCallbackSetupFunction(EVENT_METADATA_KEY, eventCallbackSetupFunction);
-
-    @Component({ selector: '.test-component' })
-    class TestComponent implements OnInit, OnChange, OnDestroy {
-        public constructor(
-            @Query()
-            private element: HTMLElement,
-            @Query({ selector: 'div', multiple: true })
-            private divs: HTMLElement[],
-            private service: TestService
-        ) {}
-
-        public onInit(): void {
-            this.service.wasInit = true;
-        }
-
-        public onDestroy(): void {
-            this.service.wasDestroyed = true;
-        }
-
-        public onChange(): void {
-            this.service.wasChanged = true;
-        }
-
-        @Event({ selector: '#click1', type: 'click', options: { debounce: 1000 } })
-        public debouncedClick(): void {
-
-        }
-
-        @Event({ selector: '#click', type: 'click' })
-        public click(): void {
-            this.service.wasClicked = true;
-        }
-    }
-
-    @Component({ selector: '.test-component-2' })
-    class TestComponent2 {
-        public constructor(
-            @Query()
-            private element: HTMLElement,
-            private name: string
-        ) {}
-    }
-
-    container.registerComponent(TestComponent);
-    container.registerComponent(TestComponent2);
-
     it('Should fail to register a component', () => {
+        const { rootElement, container } = setupDomAndContainer();
+
         document.getElementById('parent')?.appendChild(document.createElement('div'));
-        expect(() => container.registerComponent(class {})).toThrow('Component metadata not found')
+
+        expect(() => container.registerComponent(class {})).toThrow('Component metadata not found');
+        document.getElementById('app')?.remove();
     });
 
-    it ('Should update a component', () => {
-        document.querySelector('.test-component')?.appendChild(document.createElement('div'));
+    it('Should initialise a component', done => {
+        const { rootElement, container } = setupDomAndContainer();
 
-        expect(service.wasChanged).toEqual(true);
+        @Component({ selector: '.test-component' })
+        class TestComponent implements OnInit {
+            public constructor(@Query() private rootElement: HTMLElement) { }
+
+            public onInit(): void {
+                // Just check for id=one
+                if (this.rootElement.id === 'one') {
+                    expect(true).toEqual(true);
+                    document.getElementById('app')?.remove();
+                    done();
+                }
+            }
+        }
+
+        container.registerComponent(TestComponent);
     });
 
-    it('Should trigger a click event', () => {
-        document.getElementById('click')?.click();
+    it('Should update a component', done => {
+        const { rootElement, container } = setupDomAndContainer();
 
-        expect(service.wasClicked).toEqual(true);
+        let detectChange = true;
+
+        @Component({ selector: '.test-component' })
+        class TestComponent implements OnInit, OnChange {
+
+            public constructor(@Query() private rootElement: HTMLElement) { }
+
+            public onInit(): void {
+                // Just check for id=one
+                if (this.rootElement.id === 'one') {
+                    document.getElementById('one')?.appendChild(document.createElement('div'));
+                }
+            }
+
+            public onChange(mutation: Mutation): void {
+                if (!detectChange) {
+                    return;
+                }
+
+                if (mutation.type === MutationType.Updated) {
+                    expect(true).toEqual(true);
+                    done();
+
+                    detectChange = false;
+                }
+            }
+        }
+
+        container.registerComponent(TestComponent);
     });
 
-    it('Should destroy a component', async () => {
-        const element = document.querySelector('.test-component');
-        const instanceId = element?.getAttribute('instance') as string;
+    it('Should destroy a component', done => {
+        const { rootElement, container } = setupDomAndContainer();
 
-        expect(container.getComponentInstancesBySelector('.test-component')?.has(instanceId)).toEqual(true);
+        let detectDestruction = true;
 
-        element?.remove();
+        @Component({ selector: '.test-component' })
+        class TestComponent implements OnInit, OnDestroy {
+            public constructor(@Query() private rootElement: HTMLElement) { }
 
-        await new Promise((resolve) => setTimeout(resolve, 0));
+            public onInit(): void {
+                this.rootElement.remove();
+            }
 
-        expect(service.wasDestroyed).toEqual(true);
-        expect(container.getComponentInstancesBySelector('.test-component')?.has(instanceId)).toEqual(false);
+            public onDestroy(): void {
+                if (detectDestruction) {
+                    expect(true).toEqual(true);
+                    done();
+                    detectDestruction = false;
+                }
+            }
+        }
+
+        container.registerComponent(TestComponent);
+
     });
 
-    it('Should trigger invalid instance id exception', done => {
+    it('Should trigger invalid instance id error', done => {
         const logSpy = jest.spyOn(global.console, 'error');
 
         logSpy.mockImplementation((e) => {
-            expect(e).toContain('Invalid instance id 20 for component .test-component-2');
+            expect(e).toContain('Invalid instance id 20 for component .test-component');
 
             logSpy.mockRestore();
             done();
         });
 
-        const div = document.createElement('div');
-        div.classList.add('test-component-2');
-        div.setAttribute('instance', '20');
-        rootElement.appendChild(div);
-    });
+        const { rootElement, container } = setupDomAndContainer();
 
-    it('Should get component instances by selector', () => {
-       const componentInstances = container.getComponentInstancesBySelector('.test-component-2');
+        @Component({ selector: '.test-component' })
+        class TestComponent {}
 
-       expect(componentInstances).toBeInstanceOf(Map);
-       expect(componentInstances.size).toEqual(1);
-    });
+        document.getElementById('two')?.setAttribute('instance', '20');
 
-    it('Should destroy all components', async () => {
-        //service.wasDestroyed = false;
-        const rootElement = document.getElementById('app2') as HTMLElement;
-        const container = new ComponentContainer(rootElement, iocContainer);
-        container.registerCallbackSetupFunction(EVENT_METADATA_KEY, eventCallbackSetupFunction);
-
-        container.registerComponent(TestComponent);
-        container.registerComponent(TestComponent2);
-
-        rootElement.remove();
-        await new Promise((resolve) => setTimeout(resolve, 100));
-
-        expect(container.getComponentInstancesBySelector('.test-component')?.size).toEqual(0);
-        expect(container.getComponentInstancesBySelector('.test-component-2')?.size).toEqual(0);
-    });
-
-    it('Should throw callback setup function not found for key test', () => {
-        @Component({ selector: '.test-component-3' })
-        class OddComponent {
-            public onSomething() {}
+        try {
+            container.registerComponent(TestComponent);
+        } catch (e) {
+            expect((e as Error).message).toContain('Invalid instance id 20 for component .test-component');
+            done();
         }
-
-        callback({ callback: 'onSomething', key: 'test', data: '' }, OddComponent, 'onSomething');
-
-        const rootElement = document.getElementById('app3') as HTMLElement;
-        const container = new ComponentContainer(rootElement, iocContainer);
-        container.registerCallbackSetupFunction(EVENT_METADATA_KEY, eventCallbackSetupFunction);
-
-        expect(() => container.registerComponent(OddComponent)).toThrow('Callback setup function not found for key test');
-    });
-
-    afterAll(() => {
-        rootElement.remove();
     });
 });
